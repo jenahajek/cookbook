45b3abe07ec84dd4f8dae78df132b0659fc342df
jena.hajek <jena.hajek@gmail.com>
2022-05-02 22:42:53 +0200

    WIP: prepare query resolvers

--- functions/graphql/index.js
+++ functions/graphql/index.js
@@ -3,14 +3,37 @@ const { ApolloServer, gql } = require("apollo-server-lambda");
 // construct a schema, using GraphQL schema language
 const typeDefs = gql`
   type Query {
-    hello: String
+    recipes: [Recipe]!
   }
+  type Recipe {
+    id: ID!
+    text: String!
+    done: Boolean!
+  }
+  type Mutation {
+      addRecipe(text: String!): Recipe
+      updateRecipe(id: ID!): Recipe
 `;
+// to mutation types i should add code of the response, success field and a message
 
+const recipes = {};
+let recipeIndex = 0;
 // Provide resolver functions for yoru schema fields
 const resolvers = {
   Query: {
-    hello: () => "Hello world!",
+    recipes: () => Object.values(recipes),
+  },
+  Mutation: {
+    addRecipe: (_, { text }) => {
+      recipeIndex++;
+      const id = `key-${recipeIndex}`;
+      recipes[id] = { id, text, done: false };
+      return recipes[id];
+    },
+    updateRecipe: (_, { id }) => {
+      recipes[id].done = true;
+      return recipes[id];
+    },
   },
 };
 
