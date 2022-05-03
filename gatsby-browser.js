ebdee6ad7fb8ebb8172a59625b5187d2f15d6d04
jena.hajek <jena.hajek@gmail.com>
2022-05-03 02:01:00 +0200

    feat: use graphql-api to get data

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,24 @@
 /* eslint-disable import/prefer-default-export */
+import React from "react";
 import wrapWithProvider from "./wrap-with-provider";
 import "./src/styles/global.scss";
 
-export const wrapRootElement = wrapWithProvider;
+const {
+  ApolloProvider,
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+} = require("@apollo/client");
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: "https://kucharka.jenahajek.com/.netlify/functions/graphql",
+  }),
+});
+
+export const wrapRootElement = ({ element }) => (
+  <ApolloProvider client={client}>
+    {wrapWithProvider({ element })}
+  </ApolloProvider>
+);
--- src/pages/app.jsx
+++ src/pages/app.jsx
@@ -1,40 +1,66 @@
-import React, { useContext, useRef, useReducer } from "react";
+import React, { useContext, useRef } from "react";
 import { Router } from "@reach/router";
+import { gql, useMutation, useQuery } from "@apollo/client";
 import { IdentityContext } from "../../identity-context";
 
-const recipesReducer = (state, action) => {
-  switch (action.type) {
-    case "addRecipe":
-      return [{ done: false, value: action.payload }, ...state];
-    default:
-      throw new Error();
+const ADD_RECIPE = gql`
+  mutation AddRecipe($text: String!) {
+    addRecipe(text: $text) {
+      id
+    }
   }
-};
+`;
+
+const UPDATE_RECIPE = gql`
+  mutation UpdateRecipe($id: ID!) {
+    updateRecipe(id: $id) {
+      text
+      done
+    }
+  }
+`;
+
+const GET_RECIPES = gql`
+  query GetRecipes {
+    recipes {
+      id
+      text
+      done
+    }
+  }
+`;
 
 const Dash = () => {
   const { user } = useContext(IdentityContext);
-  const [recipes, dispatch] = useReducer(recipesReducer, []);
   const inputRef = useRef();
+  const [addRecipe] = useMutation(ADD_RECIPE);
+  const [updateRecipe] = useMutation(UPDATE_RECIPE);
+  const { loading, error, data, refetch } = useQuery(GET_RECIPES);
 
   return (
     <div>
       Dash {user && user.user_metadata.full_name}
       <form
-        onSubmit={(e) => {
+        onSubmit={async (e) => {
           e.preventDefault();
-          dispatch({ type: "addRecipe", payload: inputRef.current.value });
+          await addRecipe({ variables: { text: inputRef.current.value } });
           inputRef.current.value = "";
+          await refetch();
         }}>
-        <label>
+        <label htmlFor="shutuplinter">
           jmeno
-          <input ref={inputRef} type="text" />
+          <input ref={inputRef} type="text" id="shutuplinter" />
         </label>
         <button type="submit">Submit</button>
       </form>
       <div>
-        {recipes.map((recipe, index) => (
-          <div key={index}>{recipe.value}</div>
-        ))}
+        {loading ? <div>loading...</div> : null}
+        {error ? <div>{error.message}</div> : null}
+        {!loading &&
+          !error &&
+          data.recipes.map((recipe) => (
+            <div key={recipe.id}>{recipe.text}</div>
+          ))}
       </div>
     </div>
   );
