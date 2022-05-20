4ad2b4410dc01abca9768227e67fe0f4bb38f3af
jena.hajek <jena.hajek@gmail.com>
2022-05-21 01:07:23 +0200

    debugging

--- functions/graphql/index.js
+++ functions/graphql/index.js
@@ -97,7 +97,7 @@ const server = new ApolloServer({
     if (context.clientContext.user) {
       return { user: context.clientContext.user.sub };
     }
-    return { user: context };
+    return { user: JSON.stringify(context) };
   },
   playground: true,
   introspection: true,
